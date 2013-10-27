21ecfc31e976d9c878423c1c1212ed3667ce1b5c
Jas <Jas@127.0.0.1>
2013-10-27 12:45:38 -0700

    Added IsNumber and FileName

--- src/Stock.py
+++ src/Stock.py
@@ -9,10 +9,24 @@ class Stock(object):
     Stock class will read a csv file provide a list of stock prices 
     '''
 
-
+    def IsNumber(self,rowValue):
+        ''' Please use/call this method in your script below to determine if row[1] is a numerical string or not.'''
+        try:
+            float(rowValue)
+            return True
+        except ValueError:
+            return False
+        
     def __init__(self,FileName):
         '''
        Input: FileName
        Discription: it will give us list of stock prices from csv file.
         '''
-        
+      self.StockPrices=self.GetStockPrices(FileName)
+      
+  def GetStockPrices(self,FileName): 
+      '''
+      Input: FileName
+      Discription: Getting stock prices for that FileName
+      Output: list of Stock prices
+      ''' 
