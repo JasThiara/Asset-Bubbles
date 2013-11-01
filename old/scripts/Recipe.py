4693d3fd43f6ff453b4b0e86a641ea197a50fa6f
Jas <Jas@127.0.0.1>
2013-11-01 12:40:08 -0700

    Updated new recipe

--- old/scripts/Recipe.py
+++ old/scripts/Recipe.py
@@ -11,13 +11,13 @@ class Recipe(object):
           x = Grid Points
           h_n = 1/n^(1/3)
           n = Number of stock data points
+    Ingredients: S_(ti), indicator function, grid points
    Description : sigma i =1,n-1 1_{|s_t(i)-x)| < h_n} = *
-   Recipe: step1: We run each stock price S_(ti) from * for each grid point x.
-                       a) if |S-x_i|<h_n
-                               print 1
+   Recipe: step1: for Si in S1,S2....Sn:
+                  for x in X1,X2...Xm:
+                        if |Si-x|<h_n
+                               step 2
                           else 0
-           step 2: Add all 1 points to list for each x_i
-           step 3: Count data from step 2 for each x_i
            step 4: if the ith grid point has less than Y percent of total data points, reject the grid point
     Output: Count number of data points in each x_i
 '''
