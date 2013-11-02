418cb84c8fda95db6661d6624aa6e5800594641d
Jas <Jas@127.0.0.1>
2013-11-02 12:23:53 -0700

    Updated Grid Analysis

--- src/GridAnalysis.py
+++ src/GridAnalysis.py
@@ -26,6 +26,45 @@ class GridAnalysis(object):
         self.GridPoints = self.GetGridPoints()
         self.UsableGridPoints= self.DoGridAnalysis()
         
-    def DoGridAnalysis(self):
-    
+    def DoGridAnalysis(self,T,S,x,n,h_n,Y):
+        '''
+    Input:1) T = Time from[0,T] which is for a day each mintue (60*n)
+         2) S = Stock prices
+         3) x = Grid Points
+         4) h_n = 1/n^(1/3)
+         5) n = Number of stock data points
+         6) Y =  Y percent of total data points
+    Ingredients: 1) S_(ti), 
+                 2) indicator function, 
+                 3) grid points, 
+                 4) a dictionary where the keys are the grid points and the value is a list for each grid point (used in Step 2)
+    Description : sigma i =1,n-1 1_{|s_t(i)-x)| < h_n} = *
+    Recipe:           step1: 
+                          for Si in S1,S2....Sn:
+                               for x in X1,X2...Xm:
+                                  if |Si-x|<h_n:
+                                    step 2: 
+                                    Add Si into list corresponding to x
+                           Step 3:
+                           for x in X1, X2,...,Xm: 
+                                if the list of data points corresponding to x has greater than Y% of total grid points n:
+                                     add it to the list of usable grid points
+                           Step 4:
+                           return the outputs
+   Output: 1) the list of usable grid points
+               2) for each usable grid point, the list of usable grid points
+        n=len(S)
+        '''
+            Sum = 0.0
+            for i in range len(S):
+                Sti = S[i]
+                for j in range of len(x):
+                    xj = x[m]
+                    if abs(S[i]-x[m]<h_n):
+                        sum+S[i]
+                    else nothing
+                    
+                        
+                        
+            
         
