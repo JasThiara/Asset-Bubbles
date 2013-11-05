c7d0bd5b744e8a2bd4b9f37003f7b24d4b27952a
Jas <Jas@127.0.0.1>
2013-11-04 21:55:49 -0800

    Updated Sublocaltime

--- old/scripts/Sublocaltime.py
+++ old/scripts/Sublocaltime.py
@@ -103,14 +103,13 @@ def DoGridAnalysis(self,T,S,x,n,h_n,Y):
                2) for each usable grid point, the list of usable grid points
         n=len(S)
        '''
-    d = dict()
-    x,S =d.key(),d.value()
-    for i in range (len(x)):
-            d[i] = len(x)
-                for j in range of len(S):
-                d[j]=len(S)
-                if abs(x-S)<h_n:
-                    d[j].append(S)
+    d = dict()# Creating empty dictionary 
+    x,S =d.key(),d.value()# Grid Point x is assigned as dictionary key and S is dictionary value 
+    for i in range (len(x)):# for i in length of x1,x2....xm
+        for j in range of len(S):# for j in range of S1,S2...Sn
+                d[j]=len()# adding key j and length pair to the dictionary 
+                if abs(x-S)<h_n:# satisfying the condition if true then add ith x value to corresponding d[j]
+                    d[j].append(i)
                 
 def Derive_hn(S):
     """
