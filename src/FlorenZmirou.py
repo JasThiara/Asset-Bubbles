e0e17b20ee8bdfcb8ba9e53d28c6333e7dcb7a78
Clinton Bowen <clinton.bowen@gmail.com>
2013-11-05 19:06:25 -0800

    Added DoGridAnalysis into FlorenZimirou.  Do not use SubLocalTime.py anymore. it is old code.  Please use the src folder code from now on.

--- src/FlorenZmirou.py
+++ src/FlorenZmirou.py
@@ -148,5 +148,48 @@ class FlorenZmirou(GridAnalysis):
             x.append(ex)
         return x
         
-        
-        
+    def DoGridAnalysis(self,T,S,x,n,h_n,Y):
+        '''
+        Input:1) T = Time from[0,T] which is for a day each mintue (60*n)
+             2) S = Stock prices
+             3) x = Grid Points
+             4) h_n = 1/n^(1/3)
+             5) n = Number of stock data points
+             6) Y =  Y percent of total data points
+        Ingredients: 1) S_(ti), 
+                     2) indicator function, 
+                     3) grid points, 
+                     4) a dictionary where the keys are the grid points and the value is a list for each grid point (used in Step 2)
+        Description : sigma i =1,n-1 1_{|s_t(i)-x)| < h_n} = *
+        Recipe:           step1: 
+                              for Si in S1,S2....Sn:
+                                   for x in X1,X2...Xm:
+                                      if |Si-x|<h_n:
+                                        step 2: 
+                                        Add Si into list corresponding to x
+                               Step 3:
+                               for x in X1, X2,...,Xm: 
+                                    if the list of data points corresponding to x has greater than Y% of total grid points n:
+                                         add it to the list of usable grid points
+                               Step 4:
+                               return the outputs
+        Output: 1) the list of usable grid points
+                   2) for each usable grid point, the list of usable grid points
+            n=len(S)
+        '''
+        usableGridPoints = x
+        d = dict()# Creating empty dictionary
+        stockPriceCount = float(len(S)) 
+        for gridPoint in x:# The grid points are your keys
+            d[gridPoint]=list()#a dictionary where the keys are the grid points and the value is a list for each grid point (used in Step 2)
+            for stockPrice in S:# stock price in S    
+                if  abs(gridPoint-stockPrice)<h_n:# satisfying the condition if true then add x value to corresponding Si
+                    d[gridPoint].append(stockPrice)#Note the number of points for the gridPoint is len(d[gridPoint])
+        for gridPoint in x:
+            listOfPointsForGridPoint = d[gridPoint]
+            numberOfPointsInList = float(len(listOfPointsForGridPoint))
+            percentOfStockPrices = numberOfPointsInList / stockPriceCount
+            if  percentOfStockPrices < Y:# We just want to remove the number grid points from the dictionary since we have all data.
+                d.pop(gridPoint)#pop(key[, default]) If key is in the dictionary, remove it and return its value, else return default. If default is not given and key is not in the dictionary, a KeyError is raised.
+                usableGridPoints.pop(gridPoint)
+        return usableGridPoints, d# 1) the list of usable grid points 2) for each usable grid point, the list of usable grid points
