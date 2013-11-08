22a97887054c979b5da6d170d5673cd199fcaf54
Clinton Bowen <clinton.bowen@gmail.com>
2013-11-07 17:13:05 -0800

    Adding a unitTest for the florenzmirou class

---
+++ src/UnitTestFlorenZimirou.py
@@ -0,0 +1,18 @@
+'''
+Created on Nov 7, 2013
+
+@author: clinton
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test']
+    unittest.main()
