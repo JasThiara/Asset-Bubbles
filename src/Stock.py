ca65b55fa6c3882bc6af4a7d4e19f32b1dcb1cd6
clinton <clinton@clinton-VirtualBox>
2013-11-13 14:24:05 -0800

    Stock now has the capaility of getting historical minute to minute data .

--- src/Stock.py
+++ src/Stock.py
@@ -4,6 +4,7 @@ Created on Oct 27, 2013
 @author: Jas
 '''
 import csv
+import urllib 
 class Stock(object):
     '''
     Stock class will read a csv file provide a list of stock prices 
@@ -25,13 +26,49 @@ class Stock(object):
             return True
         except ValueError:
             return False
-        
-    def __init__(self,FileName):
+    def GetGoogleData(self,Parameters):
         '''
-        Input: FileName
+        Input
+        Parameters = [ticker,days,period]
+        1)the first element in the list is a string of the ticker symbol, e.g. 'appl'
+        2)the second element in the list is the historical data period, e.g. 10
+        3)the third element in the list is the period of data in seconds, e.g. 60 (seconds)
+        '''
+        link = 'http://www.google.com/finance/getprices?i=%d&p=%dd&f=d,o,h,l,c,v&df=cpct&q=%s'%(Parameters[2],Parameters[1],Parameters[0])
+        filePtr = urllib.urlopen(link)
+        DataList = filePtr.readlines()
+        tickerData = DataList[7:-1]
+        stockPrices = []
+        for minuteData in tickerData:
+            datum = minuteData.split(',')
+            stockPrices.append(float(datum[1]))
+        return stockPrices
+        
+    def __init__(self,**kwds):
+        '''
+        Keyword Usage:
+        if filename is used, it will read the yahoo API based minute to minute data:
+        
+        Stock(filename="Filename.csv")
+        
+        - or -
+        
+        if tickerParams is used, it will use the google API to retrieve minute to minute data
+        
+        Stock(tickerParams=['appl',10,60])
+        where 
+        1)the first element in the list is a string of the ticker symbol, e.g. 'appl'
+        2)the second element in the list is the historical data period, e.g. 10
+        3)the third element in the list is the period of data in seconds, e.g. 60 (seconds)
         Description: it will give us list of stock prices from csv file.
         ''' 
-        self.StockPrices=self.GetStockPrices(FileName)
+        if 'filename' in kwds:
+            self.StockPrices=self.GetStockPrices(kwds['filename'])
+        if 'tickerParams' in kwds:
+            self.StockPrices=self.GetGoogleData(kwds['tickerParams'])
+        else:
+            raise Exception("bad paramaters")
+            
         
     def GetStockPrices(self,FileName): 
         '''
