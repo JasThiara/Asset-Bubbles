01fe0ea728bd359ec71dd8598d6aa4137f181290
Jas <Jas@127.0.0.1>
2013-11-21 18:02:54 -0800

    modified folder structure

---
+++ src/AssetBubbleDetection.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 21, 2013
+
+@author: Jas
+'''
+from sage.all import *
+class AssetBubbleDetection(object):
+    '''
+    This class performs asset bubble detection over a variety of asset bubble objects
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
